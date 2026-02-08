09791c92c3d33f78c7f4438576adbfc7f77507db
Hem Palariya <palariya8513@gmail.com>
2026-02-08 20:24:52 +0530

    routes created

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+import CreateOrder from "./features/order/CreateOrder"
+import Order from "./features/order/Order"
+
+export default function App() {
+  const router = createBrowserRouter([
+   {
+    path: "/",
+    element: <Home/>
+   }, 
+   {
+    path: "/menu",
+    element: <Menu/>
+   },
+   {
+    path: "/cart",
+    element: <Cart/>
+   },
+   {
+    path: "/order/new",
+    element: <CreateOrder/>
+   },
+   {
+    path: "/order/:orderId",
+    element: <Order/>
+   }
+  ])
+  return (
+     <RouterProvider router={router} />
+  )
+}
